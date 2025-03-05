8e45930cebd49a54c5d51cbe8725b373ed7a8aaa
Jainum Sanghavi <jainumsanghavi@Jainums-Laptop.local>
2025-03-05 15:17:37 -0500

    class variables

--- SimulatedAnnealing.py
+++ SimulatedAnnealing.py
@@ -6,7 +6,12 @@ from warehouse import build_sample_warehouse, populate_warehouse
 
 class SimulatedAnnealing:
     def __init__(self, warehouse, initial_temp=1000, cooling_rate=0.99, min_temp=1):
-        pass
+        self.warehouse = warehouse
+        self.temperature = initial_temp
+        self.cooling_rate = cooling_rate
+        self.min_temp = min_temp
+        self.best_state = None
+        self.best_cost = float('inf')
 
     def objective_function(self):
         pass
