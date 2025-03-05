54704006454fc92704da155293d99adadcf4eb97
Jainum Sanghavi <jainumsanghavi@Jainums-Laptop.local>
2025-03-05 15:16:25 -0500

    Simulated annealing rough structure

---
+++ SimulatedAnnealing.py
@@ -0,0 +1,21 @@
+import numpy as np
+import random
+import math
+from warehouse import build_sample_warehouse, populate_warehouse
+
+
+class SimulatedAnnealing:
+    def __init__(self, warehouse, initial_temp=1000, cooling_rate=0.99, min_temp=1):
+        pass
+
+    def objective_function(self):
+        pass
+    def generate_neighbor(self):
+        pass
+        
+
+    def acceptance_probability(self, old_cost, new_cost):
+        pass
+
+    def optimize(self, iterations=1000):
+        pass
